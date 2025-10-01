a8d4ce2c8724fdefa04ef6e7fdfbbf03008a1f9f
aloshvp <aloshvp12@gmail.com>
2025-10-01 23:55:01 +0530

    banner

--- src/app/components/Featured.jsx
+++ src/app/components/Featured.jsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Featured = () => {
     return (
-        <div>Featured</div>
+        <div>
+            <h1 className='title'><b>Lorem ipsum,</b> dolor sit amet consectetur adipisicing elit. </h1>
+            <div className="post">
+                <div className="imageContainer">
+                    <Image src="/p1.jpeg" alt="" width={100} height={100} />
+                </div>
+                <div className="textContainer">
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, reiciendis enim</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus labore rerum aliquam iusto culpa ex voluptate, repellendus, doloribus magni odit fugit? Quisquam impedit sed non pariatur consectetur. Aliquam, exercitationem dolorum?</p>
+                    <button>Read More</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/app/components/ThemeToggle.jsx
+++ src/app/components/ThemeToggle.jsx
@@ -20,7 +20,7 @@ const ThemeToggle = () => {
         >
             <Image src="/moon.png" alt="" width={14} height={14} />
             <div className="ball"
-                style={theme === "dark" ? { left: 1, backgroundColor: "#0f172a" } : { right: 1, backgroundColor: "#fff" }}>
+                style={theme === "dark" ? { left: 1, background: "#0f172a" } : { right: 1, background: "#fff" }}>
             </div>
             <Image src="/sun.png" alt="" width={14} height={14} />
         </div>
