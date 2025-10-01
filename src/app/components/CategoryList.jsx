cf5641f3db287951a47e630cd2131983991f3e6e
aloshvp <aloshvp12@gmail.com>
2025-10-02 00:07:57 +0530

    Update CategoryList.jsx

--- src/app/components/CategoryList.jsx
+++ src/app/components/CategoryList.jsx
@@ -1,8 +1,29 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const CategoryList = () => {
     return (
-        <div>CategoryList</div>
+        <div>
+            <h1>Popular Categories</h1>
+            <div className="categoryList">
+                <Link href="/blog?cat=style">
+                    <Image src="/style.png" alt="category1" width={32} height={32} className='image' />
+                </Link>
+                <Link href="/blog?cat=style">
+                    <Image src="/fashion.png" alt="category1" width={32} height={32} className='image' />
+                </Link>
+                <Link href="/blog?cat=style">
+                    <Image src="/food.png" alt="category1" width={32} height={32} className='image' />
+                </Link>
+                <Link href="/blog?cat=style">
+                    <Image src="/style.png" alt="category1" width={32} height={32} className='image' />
+                </Link>
+                <Link href="/blog?cat=style">
+                    <Image src="/style.png" alt="category1" width={32} height={32} className='image' />
+                </Link>
+            </div>
+        </div>
     )
 }
 
