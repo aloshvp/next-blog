c4f2c79dbb7cefd4d9e656bfab2cc70cdca7af0b
aloshvp <aloshvp12@gmail.com>
2025-10-04 00:03:30 +0530

    style

--- src/app/components/CategoryList.jsx
+++ src/app/components/CategoryList.jsx
@@ -4,23 +4,28 @@ import React from 'react'
 
 const CategoryList = () => {
     return (
-        <div>
+        <div className='categoryListWrapper'>
             <h1>Popular Categories</h1>
             <div className="categoryList">
-                <Link href="/blog?cat=style">
+                <Link href="/blog?cat=style" className='categoryItem style'>
                     <Image src="/style.png" alt="category1" width={32} height={32} className='image' />
+                    Style
                 </Link>
-                <Link href="/blog?cat=style">
+                <Link href="/blog?cat=style" className='categoryItem fashion'>
                     <Image src="/fashion.png" alt="category1" width={32} height={32} className='image' />
+                    Fashion
                 </Link>
-                <Link href="/blog?cat=style">
+                <Link href="/blog?cat=style" className='categoryItem food'>
                     <Image src="/food.png" alt="category1" width={32} height={32} className='image' />
+                    Food
                 </Link>
-                <Link href="/blog?cat=style">
+                <Link href="/blog?cat=style" className='categoryItem travel'>
                     <Image src="/style.png" alt="category1" width={32} height={32} className='image' />
+                    Travel
                 </Link>
-                <Link href="/blog?cat=style">
+                <Link href="/blog?cat=style" className='categoryItem culture'>
                     <Image src="/style.png" alt="category1" width={32} height={32} className='image' />
+                    Culture
                 </Link>
             </div>
         </div>
