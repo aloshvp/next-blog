d03270edc59a7899a5d4870d925c253648c07256
aloshvp <aloshvp12@gmail.com>
2025-10-05 23:57:02 +0530

    style

--- src/app/components/CardList.jsx
+++ src/app/components/CardList.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import Pagination from './Pagination'
+import Image from 'next/image'
 
 const CardList = () => {
     return (
-        <div>
+        <div className='cardListWrap'>
+            <h1>Recent Blogs</h1>
+            <div className="cardListPosts">
+                <div className="cardListPost">
+                    <div className="imageContainer">
+                        <Image src="/p1.jpeg" alt="" width={600} height={500} className='image' />
+                    </div>
+                    <div className="textContainer">
+
+                    </div>
+                </div>
+            </div>
             <Pagination />
         </div>
     )
